ff8cbff96a6c0a205b3d6fbcb95905919ff00d76
SimpleMarik <mortelle.voile@gmail.com>
2018-10-10 15:35:14 +0300

    Домашнее задание первого урока

---
+++ Lesson_1/script.js
@@ -0,0 +1,27 @@
+
+function grades() {
+    let tC = prompt("Введите градусы в цельсиях:");
+    let tF = (9 / 5) * tC + 32;
+    alert ("Градусы по Фаренгейту: " + tF);
+    console.log (typeof tC);
+}
+
+function nameCopy () {
+    let admin;
+    let name = prompt("Введите имя администратора:");
+
+    admin = name;
+
+    console.log (typeof name);
+
+    if (name == 0) {
+        alert("Постарайтесь и введите что-нибудь");
+    } else {
+        alert("Админа будут звать: " + admin);
+    }
+
+}
+
+function jsqstn() {
+    alert("Результат: " + 1000 + "108");
+}
