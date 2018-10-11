0e7823370a30fd9fa69a45ca6fa0ab3642522a65
SimpleMarik <mortelle.voile@gmail.com>
2018-10-11 22:44:43 +0300

    22:44

--- Lesson_2/script.js
+++ Lesson_2/script.js
@@ -1,6 +1,17 @@
-function test() {
-    let i;
-    i = 1;
-    i++;
-    alert(i);
+
+
+function test(a) {
+    a = +prompt("Введите число меньше 15 для срабатывания счётчика до 15:");
+    switch (a) {
+        case a:
+            function hard(a) {
+                if (a < 15) {
+                    alert(a);
+                    a = hard(++a);
+                }
+                return a;
+            }
+            alert(hard(a));
+            break;
+    }
 }
